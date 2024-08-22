182d42bb0f4c36f0d65c3006593ddde796c49c20
Vinicius Marchioni <viniciusmarchionii@gmail.com>
2024-08-22 12:27:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+#myChatBot.loadModel()
+
+#criar o modelo
+myChatBot.createModel()
+
+
+
+
+print("Bem vindo ao Chatbot")
+
+
+pergunta = input("Como posso te ajudar?\n")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("Posso lhe ajudar com algo a mais?\n")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
